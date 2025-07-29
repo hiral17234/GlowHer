f1120bab549f6156af54d942b35786e05cf86c5c
Hiral Goyal 17 <goyalbalkrishna9@gmail.com>
2025-07-29 12:44:03 +0000

    in pick a date , make it easy for user to select year which is more more

--- src/app/personal-details/page.tsx
+++ src/app/personal-details/page.tsx
@@ -119,6 +119,9 @@ export default function PersonalDetailsPage() {
                                                 </PopoverTrigger>
                                                 <PopoverContent className="w-auto p-0" align="start">
                                                     <Calendar
+                                                        captionLayout="dropdown-buttons"
+                                                        fromYear={1900}
+                                                        toYear={new Date().getFullYear()}
                                                         mode="single"
                                                         selected={field.value}
                                                         onSelect={field.onChange}
