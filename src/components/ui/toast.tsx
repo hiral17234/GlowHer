1987b0f3deeb71f691d8911bef626f9a06f1adc5
Hiral Goyal 17 <goyalbalkrishna9@gmail.com>
2025-07-30 17:52:38 +0000

    make it to 0 blur and o transparent

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -30,7 +30,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-white text-black shadow-lg",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
