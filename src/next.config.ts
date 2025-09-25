677a332d3a7b434dcd30dc5c1fa33147b379fafb
Hiral Goyal 17 <goyalbalkrishna9@gmail.com>
2025-09-25 14:58:36 +0000

    now what ahppened?

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.babycenter.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
